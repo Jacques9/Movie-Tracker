3e564f7faa6849b4e55221df16a9e3eb3c44b8f9
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2023-05-14 21:28:46 +0300

    script to get movies into a json

---
+++ get_movies.py
@@ -0,0 +1,71 @@
+import requests, time, sys, json
+sys.path.append('/home/norby/Coding/Projectz/Movie_Analytics/')
+from config import API_KEY
+
+def setup():
+    BASE_URL = 'https://api.themoviedb.org/3'
+    DISCOVER_URL = f'{BASE_URL}/discover/movie'
+    GENRE_URL = f'{BASE_URL}/genre/movie/list'
+
+    params = {
+        'api_key': API_KEY,
+        'sort_by': 'popularity.desc',
+        'page': 1,
+        'vote_count.gte': 1000,
+        'language': 'en-US'
+    }
+
+    genre_response = requests.get(GENRE_URL, params={'api_key': API_KEY})
+    genre_data = genre_response.json().get('genres')
+    genre_map = {genre['id']: genre['name'] for genre in genre_data}
+
+    return (
+        BASE_URL,
+        DISCOVER_URL,
+        GENRE_URL,
+        params,
+        genre_map
+    )
+
+def get_movies():
+    BASE_URL, DISCOVER_URL, GENRE_URL, params, genre_map = setup()
+    movies = []
+
+    for i in range(1, 40):
+        params['page'] = i
+        response = requests.get(DISCOVER_URL, params=params)
+        results = response.json().get('results')
+        movies.extend(results)
+        
+        for result in results:
+            genre_names = [genre_map.get(genre_id) for genre_id in result['genre_ids']]
+            movie_data = {
+                'id': result['id'],
+                'title': result['title'],
+                'original_title': result['original_title'],
+                'release_date': result['release_date'],
+                'popularity': result['popularity'],
+                'vote_average': result['vote_average'],
+                'vote_count': result['vote_count'],
+                'overview': result['overview'],
+                'poster_path': result['poster_path'],
+                'backdrop_path': result['backdrop_path'],
+                'genre_ids': result['genre_ids'],
+                'original_language': result['original_language'],
+                'adult': result['adult'],
+                'video': result['video'],
+                'genre_names': genre_names, 
+            }
+            movies.append(movie_data)
+
+        if i % 40 == 0:
+            time.sleep(10)
+
+    with open('Movie-Analytics/movies.json', 'a') as f:
+        for movie in movies:
+            json.dump(movie, f)
+            f.write('\n')
+
+if __name__ == '__main__':
+    get_movies()
+    
