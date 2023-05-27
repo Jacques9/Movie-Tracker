0bb451d009d85d7c18e93ee4591e07eb96f75d2a
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2023-05-27 14:01:19 +0300

    added some endpoints for movies

--- api/routers/movies.py
+++ api/routers/movies.py
@@ -22,3 +22,35 @@ def get_all_movies():
     )
 
     return movies
+
+@router.get('/find/{id}')
+def get_movie(id: str):
+    result = movies_collection.get_movie(id)
+
+    if result is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Movie not found'
+        )
+    
+    return {
+        '_id': str(result['_id']),
+        'title': result['title'],
+        'release_date': result['release_date'],
+        'popularity': result['popularity'],
+        'poster_path': result['poster_path'],
+        'backdrop_path': result['backdrop_path'],
+        'genre_names': result['genre_names']
+    }
+
+@router.delete('/delete/{id}')
+def delete_movie(id: str):
+    if movies_collection.delete_movie(id):
+        return {
+            'message': 'Movie deleted succesfully!'
+        }
+    else: 
+        raise HTTPException(
+            status_code=404,
+            detail='Movie not found'
+        )
