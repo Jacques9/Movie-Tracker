34f85f4b8bff7b3239b311e5f7d4b09d1ec8f7b8
Jacques9 <104220884+Jacques9@users.noreply.github.com>
2023-06-14 21:42:38 +0300

    changes

--- Backend/api/db_connect.py
+++ Backend/api/db_connect.py
@@ -1,8 +1,21 @@
-from config import DB_NAME, CON_STR
-from pymongo import MongoClient
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-client = MongoClient(CON_STR)
-db = client[DB_NAME]
+def init_firestore():
+    cred = credentials.Certificate('/home/norby/Coding/Projectz/Movie_Tracker/Movie-Tracker/movie-tracker-7ab60-firebase-adminsdk-y16h6-fac369a8a5.json')
+    firebase_admin.initialize_app(cred)
 
-users_collection = db['users']
-movies_collection = db['movies']
+    db = firestore.client()
+
+    return db
+
+def create_collection(db, collection_name):
+    collection_ref = db.collection(collection_name)
+
+    return collection_ref
+
+db = init_firestore()
+
+users_collection = create_collection(db, 'users')
+movies_collection = create_collection(db, 'movies')
