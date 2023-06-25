095c0b5d120cd71e42a37d71f0ea8693ac6382f5
Amiral Synux <bossalex9@yahoo.com>
2023-06-25 16:45:39 +0300

    frontend template

---
+++ Frontend/src/ApiManager.js
@@ -0,0 +1,35 @@
+
+
+const baseUrl = "http://localhost:8000/";
+const registerUrl = baseUrl + "user/register";
+const loginUrl = baseUrl + "user/login";
+
+
+async function registerNewUser(username,password,email){
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: username, password: password, email: email })
+    };
+    console.log(requestOptions.body);
+    const response = await fetch(registerUrl, requestOptions);
+    const data = await response.json();
+    return {response: response, data: data.JSON};
+}
+async function loginUser(email,password){
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ password: password, email: email })
+    };
+    console.log(requestOptions.body);
+    const response = await fetch(loginUrl, requestOptions);
+    const data = await response.json();
+    return {response: response, data: data.JSON};
+}
+
+const Manager = {
+    registerNewUser, 
+    loginUser
+}
+export default Manager;
---
+++ Frontend/src/pages/Register.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+
+// Router
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import Manager from '../ApiManager';
+import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [shouldRedirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (
+      username === '' ||
+      email === '' ||
+      password === '' ||
+      confirmPassword === ''
+    ) {
+      setError('Empty fields');
+      return;
+    } else if (confirmPassword !== password) {
+      setError('Passwords must be equal');
+      return;
+    }
+    setLoading(true);
+    Manager.registerNewUser(username,password,email).then(result=>{
+        if(result.response.ok){
+          setRedirect(true);
+        }else{
+          setError(result.response.statusText +":"+ result.data);
+        }
+        setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    }
+  }, [error]);
+
+  if(shouldRedirect){
+    navigate("/");
+  }
+  return (
+    <section className='flex flex-col items-center justify-center sectionHeight'>
+      <div className='flex flex-col items-center justify-center px-16 py-8 bg-gray-100 rounded-md shadow-md max-w-[600px] w-[90%]'>
+        <h1 className='py-2 text-4xl font-bold tracking-wider text-yellow-400 transition-all duration-300 border-b-4 border-black hover:tracking-widest hover:text-yellow-300'>
+          Register
+        </h1>
+        <p className='mt-2 text-sm italic text-gray-500'>
+          Register and enjoy all features
+        </p>
+        <form
+          className='flex flex-col items-center justify-center w-full gap-2 mt-8'
+          onSubmit={handleRegister}
+        >
+          <input
+            type='text'
+            value={username || ''}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder={'Username'}
+            autoComplete='true'
+            className='w-full p-4 italic rounded-md shadow-sm outline-none'
+          />
+          <input
+            type='email'
+            value={email || ''}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder={'E-mail'}
+            autoComplete='true'
+            className='w-full p-4 italic rounded-md shadow-sm outline-none'
+          />
+          <input
+            type='password'
+            value={password || ''}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder={'Password'}
+            autoComplete='true'
+            className='w-full p-4 italic rounded-md shadow-sm outline-none'
+          />
+          <input
+            type='password'
+            value={confirmPassword || ''}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder={'Confirm Password'}
+            autoComplete='true'
+            className='w-full p-4 italic rounded-md shadow-sm outline-none'
+          />
+
+          {!loading && (
+            <input
+              type='submit'
+              value='Sign in'
+              className='w-full p-4 font-bold text-white transition-all duration-300 bg-yellow-400 rounded-md shadow-sm cursor-pointer hover:bg-yellow-300 hover:tracking-wider'
+            />
+          )}
+
+          {loading && (
+            <div className='flex items-center justify-center w-full'>
+              <Loading size={'30px'} />
+            </div>
+          )}
+
+          {error && <p className='error'>{error}</p>}
+        </form>
+        <div className='flex flex-col w-full gap-2 mt-4'>
+          <p className='italic text-gray-500 '>
+            Already registered?{' '}
+            <Link
+              to='/login'
+              className='pb-1 font-bold text-yellow-400 transition-all duration-150 hover:tracking-wider hover:text-yellow-300'
+            >
+              Log in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
